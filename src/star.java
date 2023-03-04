6b2520f5eb2e05d6997044dade89425a37d93011
pjs <vkrwltn12@gmail.com>
2023-03-04 23:12:27 +0900

    "star"

---
+++ src/star.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class star {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("수를 입력해 주세요");
+        int num_1 = scan.nextInt();
+
+       for (int i = 1; i<=num_1; i++){
+            for(int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+           System.out.println();
+      }
+    }
+}
