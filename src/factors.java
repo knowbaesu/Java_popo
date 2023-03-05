00fe8c050dbf190661b125898624189d50fc9a35
pjs <vkrwltn12@gmail.com>
2023-03-05 22:10:35 +0900

    "factors"

---
+++ src/factors.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class factors {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        for (int i = 1; i <= num - 1; i++) {
+            if(num%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
